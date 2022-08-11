a30f8595d97fa3df0a055ee59a6d3f64e3fadfd4
fuyuanshun <fuyuanshun521@gmail.com>
2022-08-11 15:53:29 +0800

    修改文件生成路径

--- com/fys/conf/configuration.py
+++ com/fys/conf/configuration.py
@@ -2,7 +2,7 @@
 init_path = "http://116.204.156.73:7772/plate/"
 
 # 爬取文件的存储路径
-save_path = "D:/chat_videos2/"
+save_path = "D:/chat_videos/"
 
 # Headers-UserAgent
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 " \
